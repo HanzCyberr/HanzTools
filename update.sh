16e0a1ea237adc8352924d17bf5b0e4cdd021e8b
HanzCyberr <hnzsbck@gmail.com>
2026-04-18 07:32:13 +0700

    first commit HanzTools

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "=== HanzUpdater v1.0 ==="
+echo "Dibuat jam 04.30 oleh Hanz sang Raja Termux"
+echo ""
+echo "Update dimulai..."
+apt update && apt upgrade -y
+echo ""
+echo "Kelarr Hanz. Termux lu udah versi paling gacor."
+echo "Gak tamat sekolah? Gak masalah. Bisa ngoding."
